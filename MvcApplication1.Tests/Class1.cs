954f7c1c80a03567fe69626f2d43efe0754a1fce
Kristine Fagerli <kristine.fagerli@bekk.no>
2013-11-08 11:29:17 +0100

    added tests

---
+++ MvcApplication1.Tests/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace MvcApplication1.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void testing()
+        {
+            Assert.True(true);
+        }
+
+        [Test]
+        public void testing2()
+        {
+            Assert.Fail();
+        }
+    }
+}
